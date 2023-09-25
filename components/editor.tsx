d6c08d299ce584c8ba467abd33f43b73c7a5c442
Luca Beetz <luca-beetz@tutanota.com>
2023-09-25 22:00:20 +0200

    add editor

---
+++ components/editor.tsx
@@ -0,0 +1,7 @@
+export default function Editor() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-between p-8 bg-[#FCF6E5]">
+      <textarea className="w-1/2 h-full border-none outline-none bg-transparent resize-none" autoFocus></textarea>
+    </div>
+  )
+}
