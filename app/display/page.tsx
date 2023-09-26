570fd0ead7eeb865b9991563a10c11b815befc55
Luca Beetz <luca-beetz@tutanota.com>
2023-09-27 01:04:46 +0200

    make server component

--- app/display/page.tsx
+++ app/display/page.tsx
@@ -1,54 +1,37 @@
-"use client"
-
 import Link from "next/link"
-import { useState, useEffect } from 'react'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
-export default function Display() {
-  const [posts, setPosts] = useState([])
-  const supabase = createClientComponentClient()
+export default async function Display() {
+  const supabase = createServerComponentClient({ cookies })
 
-  useEffect(() => {
-    const updateTexts = async () => {
-      const { data: { user } } = await supabase.auth.getUser()
-      if (!user) return
+  const { data: { user } } = await supabase.auth.getUser()
+  if (!user) return
 
-      const { data, error } = await supabase
-        .from("text")
-        .select()
+  const { data, error } = await supabase
+    .from("text")
+    .select()
 
-      console.log(user)
+  console.log(user)
 
-      if (error) {
-        console.log("error getting texts", error)
-        return
-      }
+  if (error) {
+    console.log("error getting texts", error)
+    return
+  }
 
-      for (const post of data) {
-        const res = await supabase
-          .storage
-          .from('images')
-          .getPublicUrl(`public/${post.id}.png`)
+  for (const post of data) {
+    const res = await supabase
+      .storage
+      .from('images')
+      .getPublicUrl(`public/${post.id}.png`)
 
-        if (res.error) {
-          post.image = null
-          continue
-        }
-
-        post.image = res.data.publicUrl
-      }
-
-      setPosts(data)
-      console.log(data)
-    }
-
-    updateTexts()
-  }, [])
+    post.image = res.data.publicUrl
+  }
 
   return (
     <div className="w-screen mx-auto h-screen bg-[#FCF6E5] p-4">
       <div className="grid grid-cols-5 gap-4">
-        {posts.map((post, index) => (
+        {data.map((post, index) => (
           <Link key={index} href={`/text/${post.id}`}>
             <div key={index} className="w-full h-0 pb-full bg-blue-300 cursor-pointer" style={{ backgroundImage: `url('${post.image}')`, backgroundSize: 'cover' }}></div>
           </Link>
