70cb50a38c512995138b4abf370439808541db47
Luca Beetz <luca-beetz@tutanota.com>
2023-09-28 00:36:32 +0200

    add image to post

--- app/text/[id]/page.tsx
+++ app/text/[id]/page.tsx
@@ -7,10 +7,19 @@ export default async function Text({ params }: { params: { id: string } }) {
   const supabase = createServerComponentClient({ cookies })
   const { data } = await supabase.from('text').select().eq('id', Number(params.id))
 
+  const imageRes = await supabase
+    .storage
+    .from("images")
+    .getPublicUrl(`public/${params.id}.png`)
+  const imageUrl = imageRes.data.publicUrl
+
   return (
-    <div className="flex h-screen flex-col items-center justify-between p-8 bg-[#FCF6E5]">
+    <div className="flex min-h-screen flex-col items-center justify-between p-8 bg-[#FCF6E5]">
       {data.length > 0 ?
-        <p className="w-1/2  border-none outline-none bg-transparent">{data[0].content}</p>
+        <div className="flex flex-col w-full items-center justify-between">
+          <img src={imageUrl} className="w-80 pb-8"></img>
+          <p className="w-1/2  border-none outline-none bg-transparent whitespace-pre-line">{data[0].content}</p>
+        </div>
         :
         <p>Not found</p>
       }
